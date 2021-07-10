fab1a301ab709787462cdc9a66ad8637f718088f
timothy.j.dailey@gmail.com <timothy.j.dailey@gmail.com>
2021-07-10 14:02:39 -0700

    add unittests prob1

--- problem1.m
+++ problem1.m
@@ -3,19 +3,26 @@
 #Find the sum of all the multiples of 3 or 5 below 1000.
 echo off
 
-sum_mults   = 0;
+function sum_mults = sum_mult_func (multiple_of, below_val)
+  sum_mults   = 0;
+  ismult      = false;
+
+  for itr=0:(below_val-1)
+    multof = arrayfun(@(x) 0 == mod(itr,x), multiple_of);
+    ismult = any(multof);
+
+    if ismult
+      sum_mults += itr;
+    end
+  end
+
+endfunction
+
 multiple_of = [3,5];
 below_val   = 1000;
-ismult      = false;
+assert(233168 == sum_mult_func (multiple_of, below_val));
 
-for itr=0:(below_val-1)
-
-  multof = arrayfun(@(x) 0 == mod(itr,x), multiple_of);
-  ismult = any(multof);
-
-  if ismult
-    sum_mults += itr;
-  end
-end
-
-disp (sum_mults)
+%!test
+%! multiple_of = [3,5];
+%! below_val   = 1000;
+%! assert(233168 == sum_mult_func (multiple_of, below_val))    # check sum of all the multiples of 3 or 5 below 1000 =  233168
