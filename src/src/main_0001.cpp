c534161da9626a34ed207bd45a36ed2200962043
timothy.j.dailey@gmail.com <timothy.j.dailey@gmail.com>
2021-07-10 16:01:02 -0700

    rename files with 000 thousands

--- src/src/main_0001.cpp
+++ src/src/main_0001.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-#include "problem1.h"
+#include "problem_0001.h"
 
 int main(int, char**) {    
     const std::vector<int> multof   = {3,5};
     constexpr int below             = 1000;
     int res = sum_mults(multof, below);
-    return 233168 == res ? 0 : 1;
+    std::cout << "sum: " << res << std::endl;
 }
--- src/src/problem_0001.cpp
+++ src/src/problem_0001.cpp
@@ -1,4 +1,4 @@
-#include "problem1.h"
+#include "problem_0001.h"
 #include <algorithm>
 
 int sum_mults (const std::vector<int>& multiple_of, const int below_val) {
