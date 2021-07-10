9d99d17acfc7ee79c29bd13e2fecb660260ef848
timothy.j.dailey@gmail.com <timothy.j.dailey@gmail.com>
2021-07-10 15:26:51 -0700

    add cmake c++ tests and CTest

---
+++ problem1/problem1.h
@@ -0,0 +1,3 @@
+#include <vector>
+
+int sum_mults (const std::vector<int>& multiple_of, const int below_val);
--- problem1/test.cpp
+++ problem1/test.cpp
@@ -5,5 +5,5 @@ int main(int, char**) {
     const std::vector<int> multof   = {3,5};
     constexpr int below             = 1000;
     int res = sum_mults(multof, below);
-    std::cout << "sum: " << res << std::endl;
+    return 233168 == res ? 0 : 1;
 }
